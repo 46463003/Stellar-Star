afd63a7328417420855565febc0b500c225aa5c6
Sze Chan 46463003 <sze.chan1@students.mq.edu.au>
2023-08-21 16:56:41 +1000

    Added main() function into displacement map

--- GUI.py
+++ GUI.py
@@ -1,16 +1,17 @@
 import bpy
-#import Displacement_Map
+import Displacement_Map
 
 class MESH_OT_add_Displacement_Map(bpy.types.Operator):
     bl_idname = "mesh.displacement_map_add"
     bl_label = "Create 3D Displacement Map"
-    #Displacement_Map
+    Displacement_Map.main()
 
 class VIEW3D_PT_CUSTOM_PANEL(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_label = "Displacement_Map"
     bl_category = "Displacement_Map"
+    
 
     def draw(self, context):
         row = self.layout.row()
