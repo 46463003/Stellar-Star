4e5b019c619c7aa51b138bde9ce571dc908537bf
abhishekpurwaha <69105512+abhishekpurwaha@users.noreply.github.com>
2023-09-07 14:14:11 +1000

    Update GUI.py
    attempt to add file explorer button

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import bpy
 import Displacement_Map
+from bpy_extras.io_utils import ImportHelper #
 import Displacement_Map_split_by_channel
 
 class MESH_OT_ADD_DISPLACEMENT(bpy.types.Operator):
@@ -17,6 +18,7 @@ class VIEW3D_PT_CUSTOM_PANEL(bpy.types.Panel):
     bl_region_type = "UI"
     bl_label = "Displacement_Map"
     bl_category = "Displacement_Map"
+    filename_ext = "*.png;*.jpg;*.jpeg;*.bmp;*.tif;*.tiff" #
 
     def draw(self, context):
         row = self.layout.row()
@@ -27,6 +29,9 @@ class VIEW3D_PT_CUSTOM_PANEL(bpy.types.Panel):
         
         row = self.layout.row()
         row.operator("mesh.colour_split_add", text = "Add Colour Split")
+        
+        row = layout.row() #
+        row.operator("wm.open_mainfile", text="Select Image").filepath = "" #
 
 
 bpy.utils.register_class(VIEW3D_PT_CUSTOM_PANEL)
