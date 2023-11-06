2a56ae01f0b9658db797c61463a22db790932a1e
Sze Chan 46463003 <sze.chan1@students.mq.edu.au>
2023-11-06 13:57:59 +1100

    Edited code

---
+++ __init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name" : "Stellar Star",
+    "author" : "Innovation Labs",
+    "description" : "For astronomers",
+    "blender" : (3, 6, 1),
+    "version" : (0, 0, 1),
+    "location" : "View3D > UI panel > Stellar",
+    "categroy" : "Object"
+}
